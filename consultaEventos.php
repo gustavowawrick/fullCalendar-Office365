4a017919085ea0e9234c1270b34dee0cf6b0f02e
Júlio Gomes Corrêa <julio.correa@itsgroup.com.br>
2024-04-29 17:59:27 -0300

    Funções para resposta de aceite

--- consultaEventos.php
+++ consultaEventos.php
@@ -1,10 +1,58 @@
 <?php
 
 error_reporting(E_ERROR | E_PARSE);
+ini_set('display_error', true);
 
 date_default_timezone_set('America/Sao_Paulo');
 
 session_start();
+
+function responseStatus($eventId, $response)
+{
+    $accessToken = $_SESSION['access_token'];
+
+    $status = [
+        'accepted' => 'accept',
+        'declined' => 'decline',
+        'tentativelyAccepted' => 'tentativelyAccept'
+    ];
+
+    // Endpoint da API do Microsoft Graph para obter os eventos do calendário do usuário
+    $graphApiEndpoint = 'https://graph.microsoft.com/v1.0/me/events/' . $eventId . '/' . $status[$response];
+
+    // Configura o cabeçalho da requisição
+    $headers = [
+        'Authorization: Bearer ' . $accessToken, 
+        'Prefer: outlook.timezone = "America/Sao_Paulo"',
+        'Content-type: application/json'
+    ];
+
+    $data = [
+        'sendResponse' => true
+    ];
+
+    // Configura a solicitação para a API do Microsoft Graph
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $graphApiEndpoint);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // Desativa a verificação do host SSL
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Desativa a verificação do peer SSL
+
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PATCH");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+    // Executa a solicitação para obter os eventos do calendário do usuário
+    $response = curl_exec($ch);
+
+    // Verifica se ocorreu algum erro durante a solicitação cURL
+    if (curl_errno($ch)) {
+        echo 'Erro ao obter eventos do Office 365: ' . curl_error($ch);
+    } else {
+        return json_decode($response);
+    }
+}
+
 
 function getUser()
 {
@@ -25,7 +73,6 @@ function getUser()
 
     // Executa a solicitação para obter os eventos do calendário do usuário
     $response = curl_exec($ch);
-    var_dump($response);
 
     // Verifica se ocorreu algum erro durante a solicitação cURL
     if (curl_errno($ch)) {
@@ -34,6 +81,41 @@ function getUser()
         return json_decode($response);
     }
 }
+
+
+function updateEventAttendees($eventId, $attendees){
+    $accessToken = $_SESSION['access_token'];     
+    // Endpoint da API do Microsoft Graph para atualizar o evento do calendário do usuário
+    $graphApiEndpoint = 'https://graph.microsoft.com/v1.0/me/calendar/events/' . $eventId;     
+    // Configura a solicitação para a API do Microsoft Graph
+    $ch = curl_init();     
+    curl_setopt($ch, CURLOPT_URL, $graphApiEndpoint);     
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $accessToken, 'Content-Type: application/json', 'Prefer: outlook.timezone = "America/Sao_Paulo"']);     
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); 
+    
+    // Desativa a verificação do host SSL
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+    // Desativa a verificação do peer SSL
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PATCH");     
+    
+    // Corpo da solicitação para atualizar o status de resposta dos participantes do evento
+    $data = [         
+        'attendees' => $attendees    
+    ];     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));     
+    
+    // Executa a solicitação para atualizar o status de resposta dos participantes do evento no calendário do Office 365
+    $response = curl_exec($ch);     
+    
+    // Verifica se ocorreu algum erro durante a solicitação cURL
+    if (curl_errno($ch)) {         
+        return ['error' => 'Erro ao atualizar status de resposta dos participantes do evento: ' . curl_error($ch)];     
+    } else {         
+        return json_decode($response);     
+    } 
+}
+
 
 function calendarEvents($action = 'GET', $eventId = '', $pageToken = null)
 {
@@ -55,25 +137,40 @@ function calendarEvents($action = 'GET', $eventId = '', $pageToken = null)
 
     $graphApiEndpoint .= '?' . http_build_query($queryParams);
 
+    $headers = [
+        'Authorization: Bearer ' . $accessToken, 
+        'Prefer: outlook.timezone = "America/Sao_Paulo"',
+        'Content-type: application/json'
+    ];
+
     // Configura a solicitação para a API do Microsoft Graph
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $graphApiEndpoint);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $accessToken, 'Prefer: outlook.timezone = "America/Sao_Paulo"']);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // Desativa a verificação do host SSL
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Desativa a verificação do peer SSL
 
     if ($action == 'DELETE') {
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-    } else if ($action == 'RESPONSE') {
+    } else if ($action == 'PATCH') {
+
         $responseUser = getUser();
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PATCH");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, [
-            'attendees' => [
-                'status' => ['response' => $_POST['response'],['time' => date('Y-m-d H:i:s')]],
-                'emailAddress' => ['address' => $responseUser->{'mail'},['name' => $responseUser->{'name'}]],
+
+        $data = [
+            'attendees' => [                
+                'status' => [
+                    'response' => $_POST['response']
+                ],
+                'emailAddress' => [
+                    'address' => $responseUser->{'mail'},
+                    'name' => $responseUser->{'displayName'}
+                ],
             ]
-        ]);
+        ];
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PATCH");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
     } else {
         curl_setopt($ch, CURLOPT_POST, false);
     }
@@ -89,7 +186,9 @@ function calendarEvents($action = 'GET', $eventId = '', $pageToken = null)
     }
 }
 
-$action = $_POST['action'];
+$action = filter_input('PATCH', 'action');
+
+echo "ACTION: " . $action . "<br/>";
 
 // Verifica se foi enviada uma solicitação de exclusão de evento
 if (isset($_POST['eventId']) && $action == 'DELETE') {
@@ -97,10 +196,9 @@ if (isset($_POST['eventId']) && $action == 'DELETE') {
     $response = calendarEvents('DELETE', $eventId);
 
     echo json_encode($response);
-} else if (isset($_POST['eventId']) && $action == 'RESPONSE') {
-    $eventId = $_POST['eventId'];
-    $response = calendarEvents('PATCH', $eventId);
-
+} else if (isset($_GET['eventId']) && $action == 'RESPONSE') {
+    $eventId = $_GET['eventId'];
+    $response = responseStatus($eventId, $_GET['response']);
     echo json_encode($response);
 } else {
     // Se não foi enviada uma solicitação de exclusão, retorna os eventos normais
