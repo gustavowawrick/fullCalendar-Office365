7fdb58f7e394306beec1e4bbf1c31e70431b00cf
Júlio Gomes Corrêa <julio.correa@itsgroup.com.br>
2024-04-29 22:10:41 -0300

    Funções para resposta de aceite

--- Calendar.php
+++ Calendar.php
@@ -2,8 +2,9 @@
 
 class Calendar{
 
-    private $graphApiEndpointEvents = 'https://graph.microsoft.com/v1.0/me/calendar/events';
-    private $graphApiEndpointEventsResponse = 'https://graph.microsoft.com/v1.0/me/events/%s/%s';
+    private const GRAPH_API_ENDPOINT_EVENTS = 'https://graph.microsoft.com/v1.0/me/calendar/events';
+    private const GRAPH_API_ENDPOINT_EVENTS_RESPONSE = 'https://graph.microsoft.com/v1.0/me/events/%s/%s';
+    private const MAX_PAGE_LOAD = 250;
     private $accessToken;
 
     public function __construct($accessToken)
@@ -12,16 +13,16 @@ class Calendar{
 
         $action = isset($_REQUEST['action']) ? $_REQUEST['action']: '';
 
-        if ($action == 'LIST'){
-            $this->RenderFullCalendar();
-        }
-
-        if ($action == 'DELETE'){
-            $this->DeleteEvent($_POST['eventId']);
-        }
-
-        if ($action == 'RESPONSE'){
-            $this->ResponseStatus($_POST['eventId'],$_POST['response']);
+        switch ($action) {
+            case 'LIST':
+                $this->renderFullCalendar();
+                break;
+            case 'DELETE':
+                $this->deleteEvent($_POST['eventId']);
+                break;
+            case 'RESPONSE':
+                $this->responseStatus($_POST['eventId'], $_POST['response']);
+                break;
         }
     }
 
@@ -33,18 +34,18 @@ class Calendar{
         return $this->accessToken;
     }
 
-    public function GetEvents($eventId = '', $pageToken = null){
+    public function getEvents($eventId = '', $pageToken = null){
         $accessToken = $this->getAccessToken();
 
         // Endpoint da API do Microsoft Graph para obter os eventos do calendário do usuário
-        $graphApiEndpoint = $this->graphApiEndpointEvents;
+        $graphApiEndpoint = self::GRAPH_API_ENDPOINT_EVENTS;
 
         if (!empty($eventId)) {
             $graphApiEndpoint .= '/' . $eventId;
         }
 
         // Configura os parâmetros de consulta para lidar com a paginação
-        $queryParams = ['top' => 250]; // Define o número máximo de eventos por página
+        $queryParams = ['top' => self::MAX_PAGE_LOAD]; // Define o número máximo de eventos por página
 
         if (!is_null($pageToken)) {
             $queryParams['$skiptoken'] = $pageToken; // Define o token de página para recuperar a próxima página de eventos
@@ -58,31 +59,14 @@ class Calendar{
             'Content-type: application/json'
         ];
 
-        // Configura a solicitação para a API do Microsoft Graph
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $graphApiEndpoint);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // Desativa a verificação do host SSL
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Desativa a verificação do peer SSL
-        curl_setopt($ch, CURLOPT_POST, false);
-
-        // Executa a solicitação para obter os eventos do calendário do usuário
-        $response = curl_exec($ch);
-
-        // Verifica se ocorreu algum erro durante a solicitação cURL
-        if (curl_errno($ch)) {
-            throw 'Erro ao obter eventos do Office 365: ' . curl_error($ch);
-        } else {
-            return json_decode($response);
-        }
+        return $this->executeCurl($graphApiEndpoint, $headers, "GET");
     }
 
-    public function DeleteEvent($eventId){
+    public function deleteEvent($eventId){
         $accessToken = $this->getAccessToken();
 
         // Endpoint da API do Microsoft Graph para obter os eventos do calendário do usuário
-        $graphApiEndpoint = $this->graphApiEndpointEvents;
+        $graphApiEndpoint = self::GRAPH_API_ENDPOINT_EVENTS;
         $graphApiEndpoint .= '/' . $eventId;
 
         $headers = [
@@ -91,27 +75,10 @@ class Calendar{
             'Content-type: application/json'
         ];
 
-        // Configura a solicitação para a API do Microsoft Graph
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $graphApiEndpoint);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // Desativa a verificação do host SSL
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Desativa a verificação do peer SSL
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-
-        // Executa a solicitação para obter os eventos do calendário do usuário
-        $response = curl_exec($ch);
-
-        // Verifica se ocorreu algum erro durante a solicitação cURL
-        if (curl_errno($ch)) {
-            throw 'Erro ao remover eventos do Office 365: ' . curl_error($ch);
-        } else {
-            return json_decode($response);
-        }
+        return $this->executeCurl($graphApiEndpoint, $headers, "DELETE");
     }
 
-    public function ResponseStatus($eventId, $response){
+    public function responseStatus($eventId, $response){
         $accessToken = $this->getAccessToken();
 
         $status = [
@@ -121,7 +88,7 @@ class Calendar{
         ];
 
         // Endpoint da API do Microsoft Graph para obter os eventos do calendário do usuário
-        $graphApiEndpoint = sprintf($this->graphApiEndpointEventsResponse,$eventId, $status[$response]);
+        $graphApiEndpoint = sprintf(self::GRAPH_API_ENDPOINT_EVENTS_RESPONSE,$eventId, $status[$response]);
 
         // Configura o cabeçalho da requisição
         $headers = [
@@ -134,29 +101,10 @@ class Calendar{
             'sendResponse' => true
         ];
 
-        // Configura a solicitação para a API do Microsoft Graph
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $graphApiEndpoint);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // Desativa a verificação do host SSL
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Desativa a verificação do peer SSL
-
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-
-        // Executa a solicitação para obter os eventos do calendário do usuário
-        $response = curl_exec($ch);
-
-        // Verifica se ocorreu algum erro durante a solicitação cURL
-        if (curl_errno($ch)) {
-            throw 'Erro ao responder eventos do Office 365: ' . curl_error($ch);
-        } else {
-            return json_decode($response);
-        }
+        return $this->executeCurl($graphApiEndpoint, $headers, "POST", $data);
     }
 
-    public function FormatEventsFullCalendar($responseEvents){
+    public function formatEventsFullCalendar($responseEvents){
         // Formata a resposta dos eventos para ser retornada ao cliente
         $arrayItens = [];
     
@@ -167,7 +115,6 @@ class Calendar{
             $item->title = $itemCalendar->subject;
             $item->eventAuthor = $itemCalendar->organizer->emailAddress->name;
             $item->eventAuthorAddress = $itemCalendar->organizer->emailAddress->address;
-    
             $item->extendedProps = new stdClass();
     
             if ($itemCalendar->isOnlineMeeting == true) {
@@ -177,17 +124,11 @@ class Calendar{
             }
     
             $item->extendedProps->url = $itemCalendar->onlineMeeting->joinUrl;
-    
             $item->extendedProps->body = $itemCalendar->body->content;
-    
             $item->extendedProps->id = $itemCalendar->id;
-    
             $item->extendedProps->isOrganizer = $itemCalendar->isOrganizer;
-    
             $item->extendedProps->allDay = $itemCalendar->isAllDay;
-    
             $item->extendedProps->location = $itemCalendar->locations;
-    
             $item->extendedProps->attendees = [];
     
             if (isset($itemCalendar->attendees)) {
@@ -200,9 +141,36 @@ class Calendar{
         return $arrayItens;
     }
 
-    public function RenderFullCalendar(){
-        $events = $this->GetEvents();
-        echo json_encode($this->FormatEventsFullCalendar($events));
+    public function renderFullCalendar(){
+        $events = $this->getEvents();
+        echo json_encode($this->formatEventsFullCalendar($events));
+    }
+
+    private function executeCurl($endpoint, $headers, $type, $data = []){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $endpoint);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);
+
+        if (!empty($data)){
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        }else{
+            curl_setopt($ch, CURLOPT_POST, false);
+        }
+
+        // Executa a solicitação para o endpoint
+        $response = curl_exec($ch);
+
+        // Verifica se ocorreu algum erro durante a solicitação cURL
+        if (curl_errno($ch)) {
+            throw 'Erro eventos do Office 365: ' . curl_error($ch);
+        } else {
+            return json_decode($response);
+        }
     }
 
 }
